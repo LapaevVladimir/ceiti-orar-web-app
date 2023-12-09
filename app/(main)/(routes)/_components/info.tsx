9bf6388d5097a915cd7baedd0dfbd0e3b93e7c57
LapaevVladimir <lapaev.vladimir899@gmail.com>
2023-12-09 20:44:22 +0200

    added Info

--- app/(main)/(routes)/_components/info.tsx
+++ app/(main)/(routes)/_components/info.tsx
@@ -30,7 +30,7 @@ export const Info = () => {
                         <AlertDialogTitle>About</AlertDialogTitle>
                         <AlertDialogDescription>
                             The schedule was created just for fun; at the current stage there may be bugs, errors, etc.
-                            If you notice any of these, please write to me. I'll try to fix them.
+                            If you notice any of these, please write to me. I&apos;ll try to fix them.
                             <a href="https://t.me/ju57_v0v4"></a>
                         </AlertDialogDescription>
                     </AlertDialogHeader>
